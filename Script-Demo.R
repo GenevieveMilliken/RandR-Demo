36bd13188f1942f435b207d5a449cd5cbe45b319
Genevieve Milliken <genevieve.milliken@gmail.com>
2022-10-26 15:56:03 -0400

    Add code to demo script

---
+++ Script-Demo.R
@@ -0,0 +1,16 @@
+1:10
+
+mean(1:10)
+
+OneToTen <- 1:10
+
+OneToTen
+
+crusio1 <- read.csv("https://raw.githubusercontent.com/fredlapolla/computationalReproClass/master/Crusio1.csv")
+
+summary(crusio1)
+
+crusio1$bw
+
+crusio1[4,5]
+
